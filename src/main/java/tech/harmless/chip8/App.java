42829746f7e2aa6fb1c336ce608b68056fca5715
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-11-12 14:06:55 -0500

    pre-commit hook and spotbugs

--- src/main/java/tech/harmless/chip8/App.java
+++ src/main/java/tech/harmless/chip8/App.java
@@ -5,7 +5,7 @@ import com.google.gson.Gson;
 /** Hello world! */
 public class App {
 
-    private byte[] memory = new byte[4096];
+    private byte[] memory = new byte[4096]; // Test
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
