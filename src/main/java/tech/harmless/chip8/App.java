f5ca3fb7654afba40ff744af6d6b2c72c047626f
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-11-11 23:26:00 -0500

    Initial Commit

---
+++ src/main/java/tech/harmless/chip8/App.java
@@ -0,0 +1,13 @@
+package tech.harmless.chip8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
